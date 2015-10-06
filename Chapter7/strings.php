da791ab03fdf4749fde6c3a8f1c83327f9ed4520
Liam Hackett <liamjhackett@gmail.com>
2015-10-06 13:44:44 +0100

    Start chapter 7 of book 'strings'

---
+++ Chapter7/strings.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liam
+ * Date: 06/10/15
+ * Time: 13:37
+ */
+
+
+//Difference between single and double quotes
+
+//Set a string
+$value = 'Pandas';
+
+//Set string with single quotes
+$first = 'We love $value \n';
+
+//Set string with double quotes
+$second = "We love $value \n";
+
+var_dump($first);
+var_dump($second);
+
